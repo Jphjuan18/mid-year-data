0a4d2758d5dae9a252bfb5fbbdb7a123433bd4ef
Juan <jphjuan18@gmail.com>
2022-09-13 20:07:11 +0000

    dictionary from scraper

--- midYearScraper.py
+++ midYearScraper.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup as bs
 from residencyList import residency_number
 from residencyCredentials import payload, header
 
-for x in residency_number:
-    print(x)
+#for x in residency_number:
+#   sh root@45.79.52.207print(x)
 #print(residency_number)
 
 html_doc = "https://www.accp.com/resandfel/profile.aspx?mode=view&rid=7205"
@@ -17,9 +17,34 @@ soup = bs(res.content,'html.parser')
 #print(soup.text)
 
 Content = soup.find(id="tblOther")
-print(Content.text)
-#print(Content)
-#for element in Content:
- #   print(element.string)
-#print(soup.prettify())
 
+Content_header = soup.find(id="ctl00_master_body_lblHeader")
+header_text = Content_header.text
+
+Content_h4 = Content.find_all("h4")
+h4_text = []
+for i in Content_h4:
+    h4_text.append(i.text)
+
+Content_span = Content.find_all("span")
+span_text = []
+for i in Content_span:
+    span_text.append(i.text)
+
+print('$$$$$$$$$$$$$$$$$$$$$')
+ 
+print(header_text)
+#for i in range(0,len(Content_h4)):
+    #print(h4_text[i])
+    #print(span_text[i])
+    #print('---')
+
+
+residency_dict = dict(zip(h4_text, span_text))
+print(residency_dict)
+
+print('$$$$$$$$$$$$$$$$$$$$$')
+for key, value in residency_dict.items():
+        print(key, ' : ', value)
+
+print('$$$$$$$$$$$$$$$$$$$$$')
