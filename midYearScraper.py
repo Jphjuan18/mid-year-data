aefc4c8172e6f9948441cc880ab06dea268c7bdb
Jphjuan18 <87881079+Jphjuan18@users.noreply.github.com>
2022-09-13 21:57:56 -0600

    Completed scraper with residency

--- midYearScraper.py
+++ midYearScraper.py
@@ -1,13 +1,14 @@
 
 import requests as req
 from bs4 import BeautifulSoup as bs
-from residencyList import residency_number
+from residencyList import residency_number, fellowship_number
 from residencyCredentials import payload, header
 import pandas as pd
 import time
 import random
 
-html_doc = f"https://www.accp.com/resandfel/profile.aspx?mode=view&rid={4514}"
+html_doc = f"https://www.accp.com/resandfel/profile.aspx?mode=view&rid={9550}" # Residency: 4514 Fellowship: 9550
+
 
 res = req.get(html_doc, data = payload, headers = header, allow_redirects=True)
 print(res)
@@ -35,7 +36,8 @@ residency_dict = dict(zip(h4_text, span_text))
     
 df = pd.DataFrame.from_dict([residency_dict], orient = "columns")
 
-for r_number in residency_number:
+#for r_number in residency_number: #for residency
+for r_number in fellowship_number: #for fellowships
     print(r_number)
 
     num = random.randrange(0,10)
@@ -45,8 +47,8 @@ for r_number in residency_number:
     html_doc = f"https://www.accp.com/resandfel/profile.aspx?mode=view&rid={r_number}"
 
     res = req.get(html_doc, data = payload, headers = header, allow_redirects=True)
-    print(res)
-    print("--")
+    #print(res)
+    #print("--")
 
     soup = bs(res.content,'html.parser')
     #print(soup.text)
@@ -68,12 +70,12 @@ for r_number in residency_number:
 
     residency_dict = dict(zip(h4_text, span_text))
           
-    print('$$$$$$$$$$$$$$$$$$$$$')        
-        
+    #print('$$$$$$$$$$$$$$$$$$$$$')           
     df.loc[len(df.index)] = residency_dict.values()        
         
 print(df)
 
-df.to_csv("midYearResidency.csv") 
+#df.to_csv("midYearFellowship.csv") 
+df.to_csv("midYearFellowship.csv") 
         # Rerun loop for second iteration
 
